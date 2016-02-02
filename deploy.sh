8c55e231a0126f340ebf698b94a161c3d27f3343
Carl Skeide <carl.skeide@sportamore.se>
2016-02-02 09:52:31 +0100

    Update gitignore/config

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd ~
-hitlist="~/dotfiles/gitconfig ~/dotfiles/screenrc"
+hitlist="~/dotfiles/screenrc"
 for file in $hitlist ~/dotfiles/bash*; do echo rm -f "~/.${file##*/}"; echo ln -s "$file" "~/.${file##*/}"; done
+echo "cat ~/dotfiles/gitconfig > ~/.gitconfig"
