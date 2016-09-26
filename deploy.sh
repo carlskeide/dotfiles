e576b094c6e6fe0e554098387465db705d5b9aca
Carl Skeide <carl@skeide.se>
2016-09-26 09:33:53 +0200

    disable login banners

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ cd ~
 hitlist="~/dotfiles/screenrc"
 for file in $hitlist ~/dotfiles/bash*; do echo rm -f "~/.${file##*/}"; echo ln -s "$file" "~/.${file##*/}"; done
 echo "cat ~/dotfiles/gitconfig > ~/.gitconfig"
+echo ": > ~/.hushlogin"
