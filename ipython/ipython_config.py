0b011c9e406a66e75bf833518ac675066fbeeec3
Carl Skeide <carl@skeide.se>
2016-09-26 09:32:25 +0200

    Improve ipython QOL

--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -1,3 +1,5 @@
+from pygments.token import Token
+
 c = get_config()
 
 c.InteractiveShellApp.exec_PYTHONSTARTUP = False
@@ -5,6 +7,7 @@ c.InteractiveShellApp.exec_PYTHONSTARTUP = False
 c.InteractiveShellApp.exec_lines = [
     'import sys, logging, json, time',
     'from datetime import datetime, timedelta',
+    'from decimal import Decimal',
     'from pprint import pprint as pp',
     'logging.basicConfig(level=logging.DEBUG)',
 ]
@@ -12,4 +15,12 @@ c.InteractiveShellApp.exec_lines = [
 c.InteractiveShell.autocall = True
 c.InteractiveShell.automagic = False
 c.InteractiveShell.autoindent = True
+
+c.TerminalInteractiveShell.highlighting_style = "monokai"
+c.TerminalInteractiveShell.highlighting_style_overrides = {
+    Token.Menu.Completions.Completion.Current: 'bg:#ffffff #000000',
+    Token.Menu.Completions.Completion: 'bg:#333333 #888888',
+    Token.Menu.Completions.ProgressButton: 'bg:#333333',
+    Token.Menu.Completions.ProgressBar: 'bg:#aaaaaa',
+}
 
