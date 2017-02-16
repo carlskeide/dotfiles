2d407db8c8d26b8234df95c328f280bf80b2d08d
Carl Skeide <carl.skeide@sportamore.se>
2017-02-16 16:32:29 +0100

    Handle config files in deploy-script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 cd ~
+
+# Literally dotfiles
 hitlist="~/dotfiles/screenrc"
 for file in $hitlist ~/dotfiles/bash*; do echo rm -f "~/.${file##*/}"; echo ln -s "$file" "~/.${file##*/}"; done
-echo "cat ~/dotfiles/gitconfig > ~/.gitconfig"
 echo ": > ~/.hushlogin"
+echo "cat dotfiles/virtualenvs.postdeactivate >>.virtualenvs/postdeactivate"
+echo "cat ~/dotfiles/gitconfig > ~/.gitconfig"
+
+# Config files
+echo "cp -aT ~/dotfiles/config/terminator/ ~/.config/terminator/"
+echo "cp -a ~/dotfiles/config/sublime-text-3/* ~/.config/sublime-text-3/Packages/User/"
