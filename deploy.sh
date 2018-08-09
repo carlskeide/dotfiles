baec02757dcd585851ff678892be60501a52e27d
Carl <carl@skeide.se>
2018-08-09 10:13:45 +0200

    Update ipython config

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,10 @@ echo "mkdir -p ~/.virtualenvs"
 echo "cat dotfiles/virtualenvs.postdeactivate >>.virtualenvs/postdeactivate"
 echo "cat ~/dotfiles/gitconfig > ~/.gitconfig"
 
+# Python
+echo "cat ~/dotfiles/ipython/ipython_config.py > ~/.ipython/profile_default/ipython_config.py"
+echo "cp -a ~/dotfiles/ipython/startup/* ~/.ipython/profile_default/startup/"
+
 # Config files
 echo "cp -aT ~/dotfiles/config/terminator/ ~/.config/terminator/"
 echo "cp -a ~/dotfiles/config/sublime-text-3/* ~/.config/sublime-text-3/Packages/User/"
--- ipython/ipython_config.py
+++ ipython/ipython_config.py
@@ -1,20 +1,35 @@
 from pygments.token import Token
+import six
 
 c = get_config()
 
 c.InteractiveShellApp.exec_PYTHONSTARTUP = False
 
+#c.InteractiveShellApp.log_level = 20
 c.InteractiveShellApp.exec_lines = [
-    'import logging, sys, os, json, time, types, collections',
-    'from datetime import datetime, timedelta',
+    'import logging, sys, os, json, types',
+    'from os import path',
+    'from time import time',
+    'from datetime import date, datetime, timedelta',
+    'from collections import defaultdict, OrderedDict, deque',
+    'from itertools import chain',
     'from decimal import Decimal',
     'from pprint import pprint as pp',
     'from IPython.utils.text import SList',
-]
+] + (
+    [
+        'from datetime import timezone',
+        'from statistics import mean, median',
+    ] if six.PY3 else [
+        'import pytz',
+    ]
+)
 
 c.InteractiveShell.autocall = True
 c.InteractiveShell.automagic = False
 c.InteractiveShell.autoindent = True
+
+c.TerminalIPythonApp.display_banner = False
 
 c.TerminalInteractiveShell.highlighting_style = "monokai"
 c.TerminalInteractiveShell.highlighting_style_overrides = {
@@ -23,4 +38,5 @@ c.TerminalInteractiveShell.highlighting_style_overrides = {
     Token.Menu.Completions.ProgressButton: 'bg:#333333',
     Token.Menu.Completions.ProgressBar: 'bg:#aaaaaa',
 }
+
 
