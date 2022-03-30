b34ac1a89d427ce8809fa51babceb8fc00a44a97
Carl Skeide <carl.skeide@sony.com>
2022-03-30 12:36:48 +0200

    Clean up and expand deployment

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,24 @@
 cd ~
 
 # Literally dotfiles
-hitlist="~/dotfiles/screenrc ~/dotfiles/tmux.conf"
+hitlist="~/dotfiles/screenrc ~/dotfiles/nanorc ~/dotfiles/tmux.conf"
 for file in $hitlist ~/dotfiles/bash*; do echo rm -f "~/.${file##*/}"; echo ln -s "$file" "~/.${file##*/}"; done
-echo ": > ~/.hushlogin"
-echo "mkdir -p ~/.virtualenvs"
-echo "cat dotfiles/virtualenvs.postdeactivate >>.virtualenvs/postdeactivate"
+
 echo "cat ~/dotfiles/gitconfig > ~/.gitconfig"
+echo "cat ~/dotfiles/dotenv > ~/.env"
+
+echo ": > ~/.hushlogin"
+
+# Custom scripts
+echo "mkdir -p ~/bin"
+for s in dotfiles/bin/*; do echo "ln -s ~/${s} ~/bin/${s##*/}"; done
 
 # Python
-echo "cat ~/dotfiles/ipython/ipython_config.py > ~/.ipython/profile_default/ipython_config.py"
-echo "cp -a ~/dotfiles/ipython/startup/* ~/.ipython/profile_default/startup/"
+echo "mkdir -p ~/.ipython/profile_default"
+echo "rm ~/.ipython/profile_default/ipython_config.py; ln -s ~/dotfiles/ipython/ipython_config.py ~/.ipython/profile_default/ipython_config.py"
+echo "rm -rf ~/.ipython/profile_default/startup; ln -s ~/dotfiles/ipython/startup ~/.ipython/profile_default/startup"
 
-# Config files
-echo "cp -aT ~/dotfiles/config/terminator/ ~/.config/terminator/"
-echo "cp -a ~/dotfiles/config/sublime-text-3/* ~/.config/sublime-text-3/Packages/User/"
+# GUI Config
+echo "mkdir -p ~/.config/sublime-text-3/Packages/User && cp -a ~/dotfiles/config/sublime-text-3/* ~/.config/sublime-text-3/Packages/User/"
+echo "mkdir -p ~/.config/rofi && ln -s ~/dotfiles/config/rofi/config.rasi ~/.config/rofi/config.rasi"
+echo "dconf load /com/gexperts/Tilix/ < ~/dotfiles/dconf/tilix"
